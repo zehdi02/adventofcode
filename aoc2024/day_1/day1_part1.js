3c9267f9659cd9d35d5025a35653a2e67634778d
zehdi02 <de.melchizedek.castro@gmail.com>
2024-12-22 00:13:31 -0500

    Fixed file reading error

--- aoc2024/day_1/day1_part1.js
+++ aoc2024/day_1/day1_part1.js
@@ -17,48 +17,47 @@
 let left_list = [];
 let right_list = [];
 
-import { readFile } from "fs";
-readFile("input.txt", "utf-8", (err, data) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
+const fs = require("fs");
+fs.readFile("input.txt", "utf-8", (err, data) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
 
-  let lines = data.trim().split("\n");
+    let lines = data.trim().split("\n");
 
-  lines.forEach((line) => {
-    let [left, right] = line.trim().split(/\s+/).map(Number);
-    left_list.push(left);
-    right_list.push(right);
-  });
+    lines.forEach((line) => {
+        let [left, right] = line.trim().split(/\s+/).map(Number);
+        left_list.push(left);
+        right_list.push(right);
+    });
 
-  let min1 = 0;
-  let min2 = 0;
-  let left_index = 0;
-  let right_index = 0;
-  let distance = 0;
-  let distances = [];
+    let min1 = 0;
+    let min2 = 0;
+    let left_index = 0;
+    let right_index = 0;
+    let distance = 0;
+    let distances = [];
 
-  while (left_list.length > 0) {
-    min1 = Math.min(...left_list);
-    min2 = Math.min(...right_list);
+    while (left_list.length > 0) {
+        min1 = Math.min(...left_list);
+        min2 = Math.min(...right_list);
 
-    distance = Math.abs(min1 - min2);
-    distances.push(distance);
+        distance = Math.abs(min1 - min2);
+        distances.push(distance);
 
-    // now, we need to remove both left and right elements that were taken account for.
-    left_index = left_list.indexOf(min1);
-    right_index = right_list.indexOf(min2);
+        // now, we need to remove both left and right elements that were taken account for.
+        left_index = left_list.indexOf(min1);
+        right_index = right_list.indexOf(min2);
 
-    left_list.splice(left_index, 1);
-    right_list.splice(right_index, 1);
-  }
+        left_list.splice(left_index, 1);
+        right_list.splice(right_index, 1);
+    }
 
-  let sum = distances.reduce(
-    (accumulator, currentValue) => accumulator + currentValue,
-    0
-  );
+    let sum = distances.reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+        0
+    );
 
-  console.log(sum);
+    console.log(sum);
 });
-
